db8bea0291b174931b93355219ac2f62ac7acb04
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-06-09 01:03:45 +0530

    Form Validation

---
+++ block-BJaaes/code/script.js
@@ -0,0 +1,35 @@
+let userNameError = "";
+
+let form = document.querySelector("form");
+
+function doesContainNumber(str){
+   return str.split("").some(e => Number(e))
+}
+function doesContain(str){
+    return str.split("").includes("@")
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let userNameElm = event.target.elements.username;
+    let name = event.target.elements.name;
+    let email = event.target.elements.email;
+    let phone = event.target.elements.Number;
+    if(userNameElm.value === ""){
+       userNameError = "Can't be empty";
+       userNameElm.nextElementSibling.innerText = userNameError;
+    }else if(userNameElm.value.length < 4){
+       userNameError = "username cannot be less than 4 characters"
+       userNameElm.nextElementSibling.innerText = userNameError;
+    }else if(doesContainNumber(name.value)){
+        userNameError = "cannot be a number"
+        name.nextElementSibling.innerText = userNameError;
+     }else if(!doesContain(email.value)){
+        userNameError = "must contain @"
+        email.nextElementSibling.innerText = userNameError
+    }else if (email.value.length < 6){
+        userNameError = "must be atleast 6 characters";
+        email.nextElementSibling.innerText = userNameError;
+    }
+}
+form.addEventListener("submit",handleSubmit);
